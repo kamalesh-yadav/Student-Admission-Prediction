169c320147ea1b95331ce049176fc08e4c6c8185
kamalesh <studentpw2@gmail.com>
2026-02-19 00:21:46 +0530

    added

--- app.py
+++ app.py
@@ -1,48 +1,38 @@
-#importing the necessary dependencies
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+import os
 import pickle
 
-app = Flask(__name__) #initializing a flask app
+app = Flask(__name__)
+CORS(app)
 
-@app.route('/',methods=['GET'])  #route to display the home page
+# Load model once
+model = pickle.load(open("admission_lr_model.pickle", "rb"))
+
+@app.route('/', methods=['GET'])
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
-@app.route('/predict',methods=['POST','GET']) #route to show the predictions in a web UI
+@app.route('/predict', methods=['POST'])
 @cross_origin()
 def index():
-    if request.method == 'POST':
-        try:
-            #reading the inputs given by the user
-            gre_score=float(request.form['gre_score'])
-            toefl_score = float(request.form['toefl_score'])
-            university_rating = float(request.form['university_rating'])
-            sop = float(request.form['sop'])
-            lor = float(request.form['lor'])
-            cgpa = float(request.form['cgpa'])
-            is_research = request.form['research']
-            if(is_research=='yes'):
-                research=1
-            else:
-                research=0
-            filename = 'admission_lr_model.pickle'
-            loaded_model = pickle.load(open(filename, 'rb')) #loading the model file from the storage
-            #predictions using the loaded model file
-            prediction=loaded_model.predict([[gre_score,toefl_score,university_rating,sop,lor,cgpa,research]])
-            print('prediction is', prediction)
-            #showing the prediction results in a UI
-            return render_template('results.html',prediction=round(100*prediction[0]))
-        except Exception as e:
-            print('The Exception message is: ',e)
-            return 'something is wrong'
-    #return render_template('results.html')
-    else:
-        return render_template('index.html')
+    try:
+        gre_score = float(request.form['gre_score'])
+        toefl_score = float(request.form['toefl_score'])
+        university_rating = float(request.form['university_rating'])
+        sop = float(request.form['sop'])
+        lor = float(request.form['lor'])
+        cgpa = float(request.form['cgpa'])
+        research = 1 if request.form['research'] == 'yes' else 0
 
+        prediction = model.predict([[gre_score,toefl_score,university_rating,sop,lor,cgpa,research]])
 
+        return render_template('result.html', prediction=round(100 * prediction[0]))
+
+    except Exception as e:
+        return str(e)
 
 if __name__ == "__main__":
-    #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True) # running the app
+     port = int(os.environ.get("PORT", 8080))
+     app.run(host="0.0.0.0", port=port)
